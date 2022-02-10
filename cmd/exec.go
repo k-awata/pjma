887cc6aed95ed23995aa579925b2721def93ce11
K.Awata <awata_kihachi@outlook.jp>
2022-02-10 11:01:05 +0900

    Add support for using quotation in scripts args

--- cmd/exec.go
+++ cmd/exec.go
@@ -5,6 +5,7 @@ Copyright © 2021 K.Awata <awata_kihachi@outlook.jp>
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
@@ -27,9 +28,46 @@ var execCmd = &cobra.Command{
 			fmt.Fprintln(os.Stderr, "script name not found in config file")
 			return
 		}
-		scr := append(strings.Fields(viper.GetString(scrname)), args[1:]...)
-		fmt.Println("> " + strings.Join(scr, " "))
-		out, err := exec.Command(scr[0], scr[1:]...).Output()
+
+		var buf bytes.Buffer
+		var quote rune
+		words := []string{}
+		esc := false
+		for _, r := range viper.GetString(scrname) {
+			if !esc && r == '\\' {
+				esc = true
+				continue
+			}
+			if esc {
+				if r != '"' && r != '\'' && r != '\\' {
+					buf.WriteRune('\\')
+				}
+				buf.WriteRune(r)
+				esc = false
+				continue
+			}
+			if r == quote {
+				quote = 0
+				continue
+			}
+			if quote == 0 && (r == '"' || r == '\'') {
+				quote = r
+				continue
+			}
+			if quote == 0 && r == ' ' {
+				if buf.Len() > 0 {
+					words = append(words, buf.String())
+					buf.Reset()
+				}
+				continue
+			}
+			buf.WriteRune(r)
+		}
+		words = append(words, buf.String())
+
+		words = append(words, args[1:]...)
+		fmt.Println("> " + strings.Join(words, " "))
+		out, err := exec.Command(words[0], words[1:]...).Output()
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
